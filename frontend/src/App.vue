0bec2abd2480e1c66c2ef99423d7ef7f0c4857fc
evheath <elliot.v.heath@gmail.com>
2022-05-26 16:38:14 -0500

    bulma

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <header>
+    <h1>Randall Reilly  Vuejs Assessment</h1>
+     <img alt="Vue logo" src="./assets/logo.png">
+    <p>By <a target="_blank" href="https://evheath.github.io/">Elliot Heath</a></p>
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
