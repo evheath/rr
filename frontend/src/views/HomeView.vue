7d3cc43a4e2fbe6a0e4cea16fccf8de588d8fedd
evheath <elliot.v.heath@gmail.com>
2022-05-26 16:46:29 -0500

    router installed

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <header>
+      <h1>Randall Reilly Vue.js Assessment</h1>
+      <p>By <a href="https://evheath.github.io/">Elliot Heath</a></p>
+    </header>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
